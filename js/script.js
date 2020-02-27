b6b5b6b4c91db0c790b780c5121950ab246e2eea
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-02-26 21:32:44 -0800

    initial pseudo code

--- js/script.js
+++ js/script.js
@@ -7,22 +7,36 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
+// I will need to manipulate the dom in particular the "quote" "source" "citation" and "year" classes
+// It would be cool to have some sort of fade in and out effect to transition the change in data but not a requierment
+// I will need to display at least five quotes
+// 
+
 /*** 
  * `quotes` array 
 ***/
-
-
+//Here I will need to make an array of objects
+/*** this object class will have a
+ * Quote
+ * person 
+ * date 'optional'
+ * citation 'optional'
+***/
 
 /***
  * `getRandomQuote` function
 ***/
-
+//obviously this function will need some type of randomising funtionality, as we all know this can be a pain in javascript
+//It will need to interact with the array of quotes
+//It will return a quote object
+//it will be intiatied like this getRandomQuote() 
 
 
 /***
  * `printQuote` function
 ***/
-
+//this will be activated upon a button click
+//the getRandomQuote function will be invoked here
 
 
 /***
@@ -30,4 +44,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false); 
