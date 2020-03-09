37b6a26da93d2c70e92678d5855eb2883d3f8dfb
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-03-09 01:36:51 -0700

    cant deal with matches

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -4,6 +4,7 @@ const addNumToArray = (array,num) => array.push(num),
       increaseNum = (num) => num += 1,
       decreaseNum = (num) => num -= 1,
       findMatch = (numA,numB) => numA === numB ? true : false;
+      addNumberToArray = (array,num) => arry.push(num)
 
 let randomArray = []
    
@@ -13,9 +14,10 @@ let randomizer = Math.floor(Math.random() * (max - 1 + 1)) + 0;
 const firstElement = randomArray[randomArray.length - 1];
 const secondElement = randomArray[randomArray.length - 2]; 
     if(arry.length >= 1 && findMatch(firstElement,secondElement) && firstElement === max) {
+        arry.push(randomizer++)
         console.log(randomizer)
     } else if (arry.length >= 1 && findMatch(firstElement,secondElement) && firstElement === max) {
-
+        arry.push(randomizer--)
         console.log(randomizer)
     } else {
         arry.push(randomizer)
