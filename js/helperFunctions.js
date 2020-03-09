2013552a1fddbc2596df16c3cb0030c538d77d8f
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-03-09 00:07:41 -0700

    random number now does not reapeat

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -1,8 +1,31 @@
 //good old helper functions 
-const addNumToArray = (array,num) => array.push(num);
-const reduceArray = (array) => array.shift();
-const increaseNum = (num) => num += 1;
-const decreaseNum = (num) => num -= 1;
-const matchNum = (numA,numB) => numA === numB ? true : false;
-//obviously this function will need some type of randomising funtionality, as we all know this can be a pain in javascript
-const randomNumber = () => Math.floor(Math.random() * (quoteAmount - 1 + 1)) + 0; 
+const addNumToArray = (array,num) => array.push(num),
+      reduceArray = (array) => array.shift(),
+      increaseNum = (num) => num += 1,
+      decreaseNum = (num) => num -= 1,
+      findMatch = (numA,numB) => numA === numB ? true : false;
+
+let randomArray = [0]
+   
+//return Math.floor(Math.random() * (max - min + 1)) + min;
+let populateRandomizerArray = (arry,max,min) => {
+let randomizer = Math.floor(Math.random() * (max - 1 + 1)) + 0;
+
+    if(arry.length >= 1){
+        const firstElement = randomArray[0];
+        const secondElement = randomArray[1]; 
+         if(findMatch(firstElement,secondElement)) {
+            increaseNum(randomizer);
+         } else if(firstElement === max && secondElement === max){decreaseNum(randomizer)}
+         else if(firstElement === min && secondElement === min){increaseNum(randomizer)}
+    }
+    arry.push(randomizer)
+}
+
+const randomNumber = () => {
+    if (randomArray.length >= 1){
+        return randomArray[randomArray.length - 2]
+    } else {
+        return randomArray[randomArray.length - 1]
+    }
+}
--- js/script.js
+++ js/script.js
@@ -8,32 +8,26 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
   //the purpose of this randomQuoteArray is to create a list of random numbers that dont match that can be used when creating the quote obj
-  let randomQuotesArray = []
 
   /***
    * `getRandomQuote` function
   ***/
- console.log(randomNumber());
-  const getRandomQuote = (rand) => quotes[rand];
-  console.log(getRandomQuote(randomNumber()))
-  
-  
-
-  //I cant allow this array to get too big 
-
+  const getRandomQuote = (rand) => quotes[randomNumber()];
+  console.log(randomNumber())
   /***
    * `printQuote` function
   ***/
   //add data to dom
    const printQuote = (quoteObject) => {
-     
+     populateRandomizerArray(randomArray,quoteAmount,0)
      quoteElement.textContent = quoteObject.quoteBody; 
      sourceElement.textContent = quoteObject.person;
      citationElement.textContent = quoteObject.citation;
+     console.log(randomNumber());
    }
-  window.onload = () => printQuote(getRandomQuote(randomNumber()));
+  window.onload = () => printQuote(getRandomQuote());
   
-
+console.log(populateRandomizerArray(randomArray,quoteAmount,0))
 
 /***
  * click event listener for the print quote button
@@ -41,5 +35,5 @@ project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('load-quote').addEventListener("click", function(){
-  printQuote(getRandomQuote(randomNumber()))
+  printQuote(getRandomQuote())
 }, false); 
