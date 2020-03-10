a3516a79010172ffc61ff8f001742b220e9fa5ce
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-03-09 21:06:27 -0700

    removing duplicates wont work

--- js/helperFunctions.js
+++ js/helperFunctions.js
@@ -7,29 +7,33 @@ const addNumToArray = (array,num) => array.push(num),
       //create random number
       getRandomNumber = () => Math.floor(Math.random() * (quotes.length - 1 + 1)) + 0;
       //remove duplicates from an array
-      removeDuplicates = (array) => array.splice(0, array.length, ...(new Set(array))),
-       // gets a random quote from the
+      removeDuplicates = (array) => [...new Set(array)]
+      /***
+      * `getRandomQuote` function
+      ***/
       getRandomQuote = (rand) => quotes[rand];
 
 //create an array to store random generated number      
 let pickedNumbersArray = []
 
 const duplicateFilter = (array) => {
-    
     const hasMoreThanOne = array.length > 1 
+
     //after cheking if the array is populated then also check for a match of the last and second to last elements of the array
     
     if(hasMoreThanOne) {
 
-    const firstElement = array[array.length - 1];
-    const secondElement = array[array.length - 2]; 
-        console.log(firstElement)
-        console.log(secondElement)
-        if(findMatch(firstElement,secondElement)) {
-            array = removeDuplicates(array);
+    let firstItem = array[array.length - 1];
+    let secondItem = array[array.length - 2]; 
+        console.log(firstItem)
+        console.log(secondItem)
+        
+        if(findMatch(firstItem,secondItem)) {
+            let newArray = removeDuplicates(array);
+            newArray.
+            return newArray 
         } 
-    }
+    } 
+    //returns an array with out any duplicates
     return array
 }
-
-pickedNumbersArray = duplicateFilter(pickedNumbersArray);
--- js/script.js
+++ js/script.js
@@ -8,20 +8,17 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
   //the purpose of this randomQuoteArray is to create a list of random numbers that dont match that can be used when creating the quote obj
-
-  /***
-   * `getRandomQuote` function
-  ***/
-
  
  const addQuotesToDom = (array) => { 
     //add a number to our basic array every time we press the button
     addNumberToArray(pickedNumbersArray,getRandomNumber());
+    const filteredArray = duplicateFilter(pickedNumbersArray);
     //pull a quote from our quotes array  
     const quoteObject = getRandomQuote(array[array.length - 1]);
     quoteElement.textContent = quoteObject.quoteBody; 
     sourceElement.textContent = quoteObject.person;
     citationElement.textContent = quoteObject.citation;
+    console.log("here is the filtered array " + filteredArray)
   }
 
   /***
@@ -37,4 +34,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote(), false); 
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
